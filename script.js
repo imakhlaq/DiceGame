f7cf592b4dfc9c1183c5507b15684ce1aa235408
imakhlaq <imakhlaqXD@gmail.com>
2022-10-29 03:48:05 +0530

    working on js

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const score2El = document.getElementById("score2");
 //current score
 const current1El = document.getElementById("current1");
 const current2El = document.getElementById("current2");
-//getting player Names
+//getting player active
 const player1El = document.getElementById("player1");
 const player2El = document.getElementById("player2");
-
-const dice = document.getElementById("dice");
+//dice
+const diceEl = document.getElementById("dice");
+//dice roll button
+const btnroll = document.querySelector(".diceroll");
 
 //setting score
 score1El.textContent = 0;
@@ -23,8 +25,30 @@ const genRandomNum = function () {
 };
 
 //players final score
-let finalscore1 = 0;
-let finalscore2 = 0;
+const score = [0, 0];
+//current playing
+const activeplayer = 0; //0 means 1st player 1 means 2nd player
 //players current score
 let currentscore1 = 0;
 let currentscore2 = 0;
+
+//showing dice
+const btnrollclick = function () {
+  let rollnum = genRandomNum();
+  diceEl.classList.remove("hidden");
+
+  //manupulating dice img
+  diceEl.src = `img/dice-${rollnum}.png`;
+  //making card active
+  player1El.classList.add("active");
+  if (rollnum !== 1 && finalscore1 <= 100) {
+    currentscore1 += rollnum;
+    current1El.textContent = currentscore1;
+  } else {
+    current1El.textContent = 0;
+    finalscore1 = currentscore1;
+    score1El.textContent = finalscore1;
+    player1El.classList.remove("active");
+  }
+};
+btnroll.addEventListener("click", btnrollclick);
